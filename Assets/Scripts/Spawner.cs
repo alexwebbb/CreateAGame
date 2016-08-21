f202f0273477c24cc0184cf166bfba26d77f31f1
alexwebbb <alexwebbb@yahoo.com>
2016-08-21 16:08:14 -0500

    chapter 5

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public Wave[] waves;
+    public Enemy enemy;
+
+    Wave currentWave;
+    int currentWaveNumber;
+
+    int enemiesRemainingToSpawn;
+    int enemiesRemainingAlive;
+    float nextSpawnTime = 0;
+
+    void Start() {
+        NextWave();
+    }
+
+    void Update() {
+
+        if(enemiesRemainingToSpawn > 0 && Time.time > nextSpawnTime) {
+            enemiesRemainingToSpawn--;
+            nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
+
+            Enemy spawnedEnemy = (Enemy)Instantiate(enemy, Vector3.zero, Quaternion.identity);
+            spawnedEnemy.OnDeath += OnEnemyDeath;
+        }
+    }
+
+    void OnEnemyDeath() {
+        enemiesRemainingAlive--;
+
+        if (enemiesRemainingAlive == 0) NextWave();
+    }
+
+    void NextWave() {
+        currentWaveNumber++;
+        if (currentWaveNumber - 1 < waves.Length) {
+            print("Wave: " + currentWaveNumber);
+            currentWave = waves[currentWaveNumber - 1];
+            enemiesRemainingToSpawn = currentWave.enemyCount;
+            enemiesRemainingAlive = enemiesRemainingToSpawn; 
+        }
+    }
+
+    [System.Serializable]
+	public class Wave {
+        public int enemyCount;
+        public float timeBetweenSpawns;
+    }
+}
