a39ef889131c28d6b366fe0450b47c7579130fb1
alexwebbb <alexwebbb@yahoo.com>
2016-09-16 01:53:50 -0500

    chapter 17

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -8,6 +8,14 @@ public class Gun : MonoBehaviour {
     public float msBetweenShots = 100;
     public float muzzleVelocity = 35;
 
+    public GameObject shell;
+    public Transform shellEjector;
+    MuzzleFlash muzzleFlash;
+
+    void Start() {
+        muzzleFlash = GetComponent<MuzzleFlash>();
+    }
+
     private float nextShotTime;
 
     public void Shoot() {
@@ -15,7 +23,10 @@ public class Gun : MonoBehaviour {
         if (Time.time > nextShotTime) {
             nextShotTime = Time.time + msBetweenShots / 1000;
             Projectile newProjectile = (Projectile)Instantiate(projectile, muzzle.position, muzzle.rotation);
-            newProjectile.SetSpeed(muzzleVelocity); 
+            newProjectile.SetSpeed(muzzleVelocity);
+
+            Instantiate(shell, shellEjector.position, shellEjector.rotation);
+            muzzleFlash.Activate();
         }
     }
 }
---
+++ Assets/Scripts/MuzzleFlash.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuzzleFlash : MonoBehaviour {
+
+    public GameObject flashObject;
+    public Sprite[] flashSprites;
+    public SpriteRenderer[] spriteRenderers;
+
+    public float flashTime;
+
+	void Start () {
+        Deactivate();
+	}
+	
+    public void Activate() {
+        flashObject.SetActive(true);
+
+        int flashSpriteIndex = Random.Range(0, flashSprites.Length - 1);
+        for(int i = 0; i < spriteRenderers.Length; i++) {
+            spriteRenderers[i].sprite = flashSprites[flashSpriteIndex];
+        }
+
+        Invoke("Deactivate", flashTime);
+    }
+
+    void Deactivate() {
+        flashObject.SetActive(false);
+    }
+}
